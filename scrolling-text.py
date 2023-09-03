006ef414238a2e1e263e539afef8e3d7558f4dab
Josh Pencheon <josh.pencheon@googlemail.com>
2023-09-03 21:59:40 +0100

    kwarg arg fix
    positional arguments would need to factor
    in 'angle', which doesn't apply for bitmap
    font

--- scrolling-text.py
+++ scrolling-text.py
@@ -8,12 +8,12 @@ graphics = PicoGraphics(DISPLAY)
 message = "Hi from Josh!"
 speed = 20
 background = (  0,   0,   0)
-foreground = (  0, 250,   0)
+foreground = (  0, 150,  50)
 scale = 1
 spacing = 1
 
 graphics.set_font("bitmap8")
-width = graphics.measure_text(message, scale, spacing)
+width = graphics.measure_text(message, scale = scale, spacing = spacing)
 
 x = GalacticUnicorn.WIDTH
 y = 2
@@ -25,7 +25,7 @@ while True:
     graphics.clear()
 
     graphics.set_pen(graphics.create_pen(*foreground))
-    graphics.text(message, x, y, -1, scale, spacing)
+    graphics.text(message, x, y, wordwrap = -1, scale = scale, spacing = spacing)
 
     galactic.update(graphics)
     time.sleep(1 / speed)
