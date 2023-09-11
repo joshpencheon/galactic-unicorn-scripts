307620ea351f7bb2456face0dda85becee5ffc65
Josh Pencheon <josh.pencheon@googlemail.com>
2023-09-11 21:07:02 +0100

    bouncy++
    I don't know why anyone would want this

--- scrolling-text.py
+++ scrolling-text.py
@@ -5,7 +5,7 @@ from picographics import PicoGraphics, DISPLAY_GALACTIC_UNICORN as DISPLAY
 galactic = GalacticUnicorn()
 graphics = PicoGraphics(DISPLAY)
 
-message = "Hi from Josh!"
+message = "Oh how great it is to read in both directions"
 speed = 20
 background = (  0,   0,   0)
 foreground = (  0, 150,  50)
@@ -18,14 +18,19 @@ width = graphics.measure_text(message, scale = scale, spacing = spacing)
 x = GalacticUnicorn.WIDTH
 y = 2
 step = -1
+entering = True
 
 while True:
     lower_limit = min(GalacticUnicorn.WIDTH - width, 1)
-    upper_limit = GalacticUnicorn.WIDTH + 1
+    if entering:
+        upper_limit = GalacticUnicorn.WIDTH + 1
+    else:
+        upper_limit = max(GalacticUnicorn.WIDTH - width, 1)
     x = min(max(x + step, lower_limit), upper_limit)
 
     if x == lower_limit or x == upper_limit:
         step *= -1
+        entering = False
 
     graphics.set_pen(graphics.create_pen(*background))
     graphics.clear()
